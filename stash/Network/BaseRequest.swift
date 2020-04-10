746dd57e0d78a6b2128b6e32811226d611228321
b20160 <dev.jonathan.green@gmail.com>
2020-04-10 03:47:17 -0400

    first commit

---
+++ stash/Network/BaseRequest.swift
@@ -0,0 +1,9 @@
+//
+//  BaseRequest.swift
+//  stash
+//
+//  Created by Jonathan Green on 4/10/20.
+//  Copyright © 2020 Jonathan Green. All rights reserved.
+//
+
+import Foundation
---
+++ stash/Network/Environment.swift
@@ -0,0 +1,9 @@
+//
+//  Environment.swift
+//  stash
+//
+//  Created by Jonathan Green on 4/9/20.
+//  Copyright © 2020 Jonathan Green. All rights reserved.
+//
+
+import Foundation
